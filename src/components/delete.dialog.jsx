6aa52736d1793346ade62d191f882919b010de3d
Jason <jason@onetapaway.com>
2025-10-11 06:21:10 -0500

    feat(delete): delete animation header

--- src/components/delete.dialog.jsx
+++ src/components/delete.dialog.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { SlButton, SlButtonGroup, SlDialog } from './shoelace'
+import logo from '../assets/icon.png'
 
 export const DeleteDialog = ({ isOpen, itemToDelete, deleteItem, closeDelete }) => {
   const handleDelete = () => {
@@ -13,7 +14,19 @@ export const DeleteDialog = ({ isOpen, itemToDelete, deleteItem, closeDelete }) 
 
   return (
     <SlDialog label="Delete" open={isOpen} onSlAfterHide={handleClose}>
-      <div className="zp-mg-bt">Are you sure you want to delete this item?</div>
+      <div className="zp-mg-bt">
+        <div className="zp-delete-header-animated">
+          <div className="zp-dtrash">
+            <div className="zp-dtrash-lid" />
+            <div className="zp-dtrash-img">
+              <img src={logo} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="zp-mg-bt">
+        Are you sure you want to delete this item?
+      </div>
       <SlButtonGroup slot="footer">
         <SlButton variant="default" size="small" onClick={handleClose}>
           Close
